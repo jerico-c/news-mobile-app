bab789ceff3fb79d1aec1c27a63440e2635c9da4
Jerico Christianto <jericochristianto@gmail.com>
2025-11-16 13:03:08 +0700

    Remove API key from Constants.kt
    Removed the API key for security reasons.

--- app/src/main/java/com/example/mandirinewsapp/util/Constants.kt
+++ app/src/main/java/com/example/mandirinewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.mandirinewsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "54b0197506d648759deaeef59426c889"
+        const val API_KEY = ""
         const val BASE_URL ="https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = "500L"
         const val QUERY_PAGE_SIZE = 20
