3bb1a2c30e3aa051bec7bfad2d4ea51ce6625050
Jerico Christianto <jericochristianto@gmail.com>
2025-12-18 12:07:16 +0700

    Update Constants.kt

--- app/src/main/java/com/example/mandirinewsapp/util/Constants.kt
+++ app/src/main/java/com/example/mandirinewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.mandirinewsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = ""
+        const val API_KEY = "YOUR_API_KEY"
         const val BASE_URL ="https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = "500L"
         const val QUERY_PAGE_SIZE = 20
